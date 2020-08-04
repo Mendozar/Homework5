b65a355c18234ac06e11205944e3b276ec1108f5
rudy <argmend@gmail.com>
2020-08-04 00:43:29 -0500

    1

---
+++ main.js
@@ -0,0 +1,37 @@
+var now = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+//Store current day
+//Update innerHTML of the #currentday <p> to
+//Dayofweek, Month, Dayof month
+
+// Store current hour
+//if statement before hour color is grey
+//current hour red
+//future hours green
+
+//Create a table with rows from 9 am - 6 pm. (8 rows, 1 row per hour)
+//Three columns (left to right): The hour, The event textbox, the save button.
+
+
+//WHEN I click into a timeblock
+//THEN I can enter an event
+//Create a textArea for the middle column.
+
+
+//WHEN I click the save button for that timeblock
+//THEN the text for that event is saved in local storage
+    //Event listener for "click" the save icon for each row.
+    //Create a function for any save button click.
+    //Use the this keyword and the before pseudo class to 
+    //store the text in the text area thats associated with 
+    //the respective save icon button. Store this to text to local storage.
+    //Have the text value persist in the text area upon refresh.
+
+    //Create a localStorage name/value pair with name="lastname" and value="Smith", then retrieve the value of "lastname" and insert it into the element with id="result":
+    // Store
+    //localStorage.setItem("lastname", "Smith");
+    // Retrieve
+    //document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+    //Store a localStorage property for each unique textarea.
+    //have a unique textarea id for each. 
+    //Maybe have the same localStorage poperty name and textarea id
